339c676677450dc289f8a39c229fe2c447962b02
Ian Homer <ian@purplepip.com>
2021-02-01 08:38:09 +0000

    Journey utils

---
+++ src/slidey/journeyish.test.js
@@ -0,0 +1,40 @@
+import { travel } from "./journeyish";
+
+const journey = ["", "step1", "step2", "step3"];
+
+it("travels nowhere", () => {
+  expect(travel(journey, "step1", 0)).toBe("step1");
+});
+
+it("travels forward", () => {
+  expect(travel(journey, "step2", 1)).toBe("step3");
+});
+
+it("travels forward from start", () => {
+  expect(travel(journey, "", 1)).toBe("step1");
+});
+
+it("travels backward", () => {
+  expect(travel(journey, "step2", -1)).toBe("step1");
+});
+
+it("travels forward to end", () => {
+  expect(travel(journey, "step3", 1)).toBe("step3");
+});
+
+it("travels backward to beginning", () => {
+  expect(travel(journey, "step1", -1)).toBe("");
+});
+
+it("travels forward from unrecognised step", () => {
+  expect(travel(journey, "unrecognised-step", 1)).toBe("step3");
+});
+
+it("travels either direction from empty steps starts at beginning", () => {
+  expect(travel(journey, "", 1)).toBe("step1");
+  expect(travel(journey, "", -1)).toBe("");
+});
+
+it("travels backward from unrecognised step to beginning", () => {
+  expect(travel(journey, "unrecognised-step", -1)).toBe("");
+});
---
+++ src/slidey/journeyish.ts
@@ -0,0 +1,36 @@
+export function travel(
+  steps: string[],
+  current: string,
+  direction: number
+): string {
+  if (direction == 0) {
+    return current;
+  }
+  const currentIndex = steps.findIndex((s) => s == current);
+  let nextIndex = 0;
+  // Forward always goes forward, even if not found
+  if (direction > 0 && currentIndex < 0) {
+    nextIndex = steps.length - 1;
+  } else {
+    nextIndex = currentIndex + direction;
+    if (nextIndex < 0) {
+      nextIndex = 0;
+    } else if (nextIndex > steps.length - 1) {
+      nextIndex = steps.length - 1;
+    }
+  }
+  return steps[nextIndex];
+}
+
+export const transition = (
+  positions: number,
+  setPosition: (setter: (position: number) => number) => void
+) => (direction: number): boolean => {
+  let changed = false;
+  setPosition((position: number) => {
+    const nextPosition = position + direction;
+    changed = nextPosition > -1 && nextPosition < positions;
+    return changed ? nextPosition : position;
+  });
+  return changed;
+};
