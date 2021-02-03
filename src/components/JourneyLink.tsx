61a2f8c853602d2daf52d718e88f8c5df7c3eda6
Ian Homer <ian@purplepip.com>
2021-02-03 23:18:42 +0000

    Fix journey background colors

--- src/components/JourneyLink.tsx
+++ src/components/JourneyLink.tsx
@@ -7,8 +7,8 @@ interface JourneyLinkProps {
 }
 
 const StyledLink = styled.a<{ active: boolean }>`
-  background-color: ${(active) => (active ? "#fff" : "auto")};
-  color: ${(active) => (active ? "#000" : "auto")};
+  background-color: ${(props) => (props.active ? "#fff" : "auto")};
+  color: ${(props) => (props.active ? "#000" : "auto")};
   display: block;
   float: left;
   width: 1.5em;
